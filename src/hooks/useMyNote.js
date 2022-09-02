03e44de1849962943d31ae749139a51e31b0f5a8
chakkapon <chakkapon.jay@gmail.com>
2022-09-02 17:53:41 +0700

    feat: std crud

--- src/hooks/useMyNote.js
+++ src/hooks/useMyNote.js
@@ -3,14 +3,61 @@ import { ref, reactive } from "vue";
 import { HTTP } from "../axios";
 
 function useGetNote(initial) {
-    // reactive composition api
     const noteList = reactive({ counter: initial });
     async function getNote() {
-        const response = await HTTP.get("/notelist");
-        noteList.data = response.data
+        const response = await HTTP.get("/notelist")
+            .then((res) => {
+                noteList.data = res.data
+            })
+            .catch((e) => {
+                console.log(e);
+            });
     }
-
     return { noteList, getNote };
 }
 
-export default { useGetNote };
+function useCreateNote(title, detail) {
+    const fromNoteTitle = ref(title);
+    const fromNoteDetail = ref(detail)
+    async function createNote() {
+        await HTTP.post("/notelist", { title: this.fromNoteTitle, detail: this.fromNoteDetail })
+            .then((res) => {
+                fromNoteTitle.value = ''
+                fromNoteDetail.value = ''
+                this.getNote()
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+    return { fromNoteTitle, fromNoteDetail, createNote };
+}
+
+async function useDeleteNote(objNote) {
+    const noteId = reactive(objNote);
+    await HTTP.delete("/notelist", { data: objNote })
+        .then((res) => {
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+}
+
+async function useUpdateNote(fromNoteId, fromNoteTitle, fromNoteDetail) {
+    let tmpEdit = {
+        note_id: fromNoteId,
+        title: fromNoteTitle,
+        detail: fromNoteDetail,
+    }
+    await HTTP.put("/notelist", tmpEdit)
+        .then((res) => {
+            fromNoteTitle.value = ''
+            fromNoteDetail.value = ''
+            this.getNote()
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+}
+
+export default { useGetNote, useCreateNote, useDeleteNote, useUpdateNote };
--- src/views/MyNoteV2.vue
+++ src/views/MyNoteV2.vue
@@ -12,7 +12,7 @@
           v-for="note in noteList.data"
           :key="note.id"
         >
-          <div class="card-body">
+          <div class="card-body" @click="getSelectNote(note)">
             <h5 class="card-title">{{ note.title }}</h5>
             <p class="card-text">
               {{ note.detail }}
@@ -21,19 +21,28 @@
           <div class="card-footer">
             <small class="text-muted">{{ note.update }}</small>
           </div>
+          <button
+            type="button"
+            class="btn btn-danger btn-sm"
+            @click="getDeleteNote(note)"
+          >
+            <i class="bi bi-trash"></i>
+          </button>
         </div>
       </div>
       <div class="col-lg-8">
         <div class="input-group mb-2">
           <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon1">Title</span>
+            <span class="input-group-text" id="basic-addon1"
+              >Title {{ fromNoteId }}</span
+            >
           </div>
           <input
             type="text"
             class="form-control"
             aria-label="Username"
             aria-describedby="basic-addon1"
-            v-model="title"
+            v-model="fromNoteTitle"
           />
         </div>
         <div class="input-group mb-2">
@@ -42,24 +51,44 @@
             aria-label="With textarea"
             placeholder="Detail ..."
             rows="15"
+            v-model="fromNoteDetail"
           ></textarea>
         </div>
-        <div class="input-group mb-2">
+        <div class="input-group mb-2" v-if="!fromNoteId">
           <button
             type="button"
             class="btn btn-success btn-xl btn-block"
             style="width: 100%"
+            @click="createNote"
           >
             Create Note +
           </button>
         </div>
+        <div class="input-group mb-2" v-else>
+          <button
+            type="button"
+            class="btn btn-warning btn-xl btn-block"
+            style="width: 100%"
+            @click="getUpdateNote"
+          >
+            Edit Note +-*/
+          </button>
+        </div>
+        <button
+          type="button"
+          class="btn btn-info btn-xl btn-block"
+          style="width: 100%"
+          @click="cancleNote"
+        >
+          Cancle
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
 import NoteFormV2 from "../components/NoteFormV2.vue";
 import useMyNote from "../hooks/useMyNote";
 export default {
@@ -67,14 +96,49 @@ export default {
     NoteFormV2,
   },
   setup() {
+    const fromNoteId = ref("");
     const { noteList, getNote } = useMyNote.useGetNote({});
-
     onMounted(() => {
       getNote();
     });
+    const { fromNoteTitle, fromNoteDetail, createNote } =
+      useMyNote.useCreateNote("");
+
+    function getDeleteNote(objNote) {
+      useMyNote.useDeleteNote(objNote);
+      getNote();
+    }
+
+    function getSelectNote(objNote) {
+      this.fromNoteId = objNote.note_id;
+      this.fromNoteTitle = objNote.title;
+      this.fromNoteDetail = objNote.detail;
+    }
+    function getUpdateNote() {
+      useMyNote.useUpdateNote(
+        this.fromNoteId,
+        this.fromNoteTitle,
+        this.fromNoteDetail
+      );
+      getNote();
+    }
+    function cancleNote() {
+      this.fromNoteId = "";
+      this.fromNoteTitle = "";
+      this.fromNoteDetail = "";
+    }
+
     return {
       noteList,
       getNote,
+      fromNoteId,
+      fromNoteTitle,
+      fromNoteDetail,
+      createNote,
+      getDeleteNote,
+      getSelectNote,
+      cancleNote,
+      getUpdateNote,
     };
   },
 };
